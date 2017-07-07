9d77fe859e499a5cd85b3e354e8dca37d3be6c72
Max Schild <max@deintv.de>
2017-07-08 00:07:37 +0200

    added inverse discrete Fourier transformation

--- transformation functions for images.py
+++ transformation functions for images.py
@@ -19,12 +19,33 @@ def DFT(data, width, height):
                 for j in range(0, width):
                     #value shows the intensity, e.g. of a colour of a pixel
                     value = data[i][j]
-                    #formula may be wrong
-                    coefficient += value * math.e ** ((-1) * cmath.sqrt(-1) * 2 * math.pi * (i * y + j * x)/ (height * width))
+                    #formula may be wrong, maybe change x and y in formula?
+                    coefficient += value * cmath.exp(-(1) * cmath.sqrt(-1) * 2 * math.pi * (i * y + j * x) / (height * width))
             #appending the coefficient to the coefficientArray
             coefficientsArray[y].append(coefficient)
     return coefficientsArray
 
+def iDFT(coefficientsArray, width, height):
+    #creating an Array which will store the values of the pixels 
+    data = []
+    #giving the right structure to the array
+
+    for i in range(0, height):
+        data.append([])
+
+    #creating an array of colour-values
+    for y in range(0, height):
+        for x in range(0, width):
+            #creating a single value
+            value = 0
+            for i in range(0, height):
+                for j in range(0, width):
+                    coefficient = coefficientsArray[i][j]
+                    value += coefficient * cmath.exp(cmath.sqrt(-1) * 2 * math.pi * (i * y + j * x) / (height * width))
+            value = value * (1 / (height * width)) #sometihing
+            #appending the value to the data-array
+            data[y].append(value)
+    return data
 
 #main programm
 
@@ -40,11 +61,30 @@ for i in range(0, height):
 #filling the data-array with values
 for i in range(0, height):
     for j in range(0, width):
-        data[i].append(i * 8 + j + 1)
+        if i % 2 == 0:
+            data[i].append(1)
+        else:
+            data[i].append(0)
 print(data)
 
-print(DFT(data, width, height))
-        
+print(iDFT(DFT(data, width, height), width, height))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
